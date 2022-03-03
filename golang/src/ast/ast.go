28bca1ab006b4eb651146e9093604bfdd53b4201
jongwow <jjongwoo.dev@gmail.com>
2022-03-03 23:08:10 +0900

    p.128
    1. AST 노드를 정의한다.
    2. 테스트를 작성한다.
    3. 파싱 노드를 작성해 테스트를 통과한다.
    4. 스스로에게 박수치며 자축한다.

--- golang/src/ast/ast.go
+++ golang/src/ast/ast.go
@@ -170,21 +170,21 @@ type InfixExpression struct {
 	Right    Expression
 }
 
-func (ie *InfixExpression) expressionNode() {
+func (ife *InfixExpression) expressionNode() {
 }
-func (ie *InfixExpression) String() string {
+func (ife *InfixExpression) String() string {
 	var out bytes.Buffer
 
 	out.WriteString("(")
-	out.WriteString(ie.Left.String())
-	out.WriteString(" " + ie.Operator + " ")
-	out.WriteString(ie.Right.String())
+	out.WriteString(ife.Left.String())
+	out.WriteString(" " + ife.Operator + " ")
+	out.WriteString(ife.Right.String())
 	out.WriteString(")")
 
 	return out.String()
 }
-func (ie *InfixExpression) TokenLiteral() string {
-	return ie.Token.Literal
+func (ife *InfixExpression) TokenLiteral() string {
+	return ife.Token.Literal
 }
 
 type Boolean struct {
@@ -200,4 +200,48 @@ func (b *Boolean) TokenLiteral() string {
 }
 func (b *Boolean) String() string {
 	return b.Token.Literal
+}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+	return out.String()
+}
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) expressionNode() {
+}
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
