f0bbac377eef4887c1d8e6b5baada6f42672f6f3
jongwow <jjongwoo.dev@gmail.com>
2022-03-05 23:50:05 +0900

    p.174

--- golang/src/evaluator/evaluator.go
+++ golang/src/evaluator/evaluator.go
@@ -7,8 +7,24 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	// 명령문
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// 표현식
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+	return result
+
 }
