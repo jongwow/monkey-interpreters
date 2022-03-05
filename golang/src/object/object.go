729ea50224000c96605dc49aa175934fa05c9bef
jongwow <jjongwoo.dev@gmail.com>
2022-03-05 22:44:03 +0900

    p.168

--- golang/src/object/object.go
+++ golang/src/object/object.go
@@ -4,6 +4,7 @@ import "fmt"
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type ObjectType string
@@ -23,4 +24,16 @@ func (i *Integer) Inspect() string {
 
 func (i *Integer) Type() ObjectType {
 	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
 }
