610a81d472c121302e001bbd079a10a821710fb9
jongwow <jjongwoo.dev@gmail.com>
2022-03-06 15:01:41 +0900

    p.213

--- golang/src/evaluator/evaluator_test.go
+++ golang/src/evaluator/evaluator_test.go
@@ -222,6 +222,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for idx, tt := range tests {
@@ -238,5 +242,21 @@ if (10 > 1) {
 			t.Errorf("wrong error message.[%d] expected=%q, got=%q",
 				idx, tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
