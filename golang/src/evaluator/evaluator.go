b4dbe393df62bbe69a8e05ac0eb79406c0eabd9f
jongwow <jjongwoo.dev@gmail.com>
2022-03-06 15:44:28 +0900

    p.223

--- golang/src/evaluator/evaluator.go
+++ golang/src/evaluator/evaluator.go
@@ -57,9 +57,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return right
 		}
 		return evalInfixExpression(left, node.Operator, right)
-
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	}
 	return nil
 }
