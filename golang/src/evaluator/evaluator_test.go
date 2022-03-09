6cba1f75da464ba3a580f994283610c09beb654a
JongWoo Park <jjongwoo.dev@gmail.com>
2022-03-09 22:17:47 +0900

    p.252

--- golang/src/evaluator/evaluator_test.go
+++ golang/src/evaluator/evaluator_test.go
@@ -246,6 +246,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for idx, tt := range tests {
@@ -343,6 +347,19 @@ func TestStringLiteral(t *testing.T) {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 	if str.Value != "Hello World" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
