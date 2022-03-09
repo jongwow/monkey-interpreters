e2f9f7db045cc06bf822a80cf46b1c4b09fe1584
JongWoo Park <jjongwoo.dev@gmail.com>
2022-03-10 02:23:26 +0900

    https://youtu.be/10Mz3z-W1BE

--- wasm/assets/script.js
+++ wasm/assets/script.js
@@ -3,4 +3,9 @@ const goWasm = new Go()
 WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
     .then((result)=>{
         goWasm.run(result.instance)       
+
+        document.getElementById("get-html").addEventListener("click", () => {
+            document.body.innerHTML = getHtml()
+        })
     })
+    // https://youtu.be/10Mz3z-W1BE
--- wasm/cmd/wasm/main.go
+++ wasm/cmd/wasm/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"syscall/js"
 )
 
-func main(){
-  fmt.Println("Hello Web Assembly from Go\n")
+var htmlString = `<h4>Hello, I'm an HTML snippet from Go!`
+
+func GetHtml() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return htmlString
+	})
+}
+
+func main() {
+	fmt.Println("Hello Web Assembly from Go\n")
+
+	js.Global().Set("getHtml", GetHtml()) // JS 로 편입 전에;.
 }
